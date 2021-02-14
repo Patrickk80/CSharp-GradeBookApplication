d78aaac9c62db575aae07870358fdb51f7059ccb
Patrick <pwakooijman@gmail.com>
2021-02-14 13:28:49 +0100

    Completed module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,11 +15,14 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
 
         public GradeBookType Type { get; set; }
+        
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -108,18 +111,23 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var addition = 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                addition = 1;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + addition;
                 case 'B':
-                    return 3;
+                    return 3 + addition;
                 case 'C':
-                    return 2;
+                    return 2 + addition;
                 case 'D':
-                    return 1;
+                    return 1 + addition;
                 case 'F':
-                    return 0;
+                    return 0 + addition;
             }
             return 0;
         }
